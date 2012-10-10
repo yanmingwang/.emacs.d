c2d1786de4104cdcd6ccfc10651a61eb97259034
yanmingwang <yanming.wang77@gmail.com>
2012-10-11 00:27:32 +0800

    add files

---
+++ elpa/ac-nrepl-20121008.1051/ac-nrepl-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ac-nrepl" "20121008.1051" "An auto-complete source for Clojure using nrepl completions [source: github]" (quote ((nrepl "0.1") (auto-complete "1.4"))))
---
+++ elpa/slime-fuzzy-20100404/slime-fuzzy-autoloads.el
@@ -0,0 +1,24 @@
+;;; slime-fuzzy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "slime-fuzzy" "slime-fuzzy.el" (20596 31039))
+;;; Generated autoloads from slime-fuzzy.el
+
+(add-hook 'slime-load-hook 'slime-fuzzy-init)
+
+;;;***
+
+;;;### (autoloads nil nil ("slime-fuzzy-pkg.el") (20596 31040 4308))
+
+;;;***
+
+(provide 'slime-fuzzy-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; slime-fuzzy-autoloads.el ends here
