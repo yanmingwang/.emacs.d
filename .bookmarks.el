625ccbadf8f17357d1c66bc4727704cc4a9d381a
wangyanming <root@michael.centos6.server>
2012-10-10 23:16:55 +0800

    first commit

---
+++ .bookmarks.el
@@ -0,0 +1,15 @@
+;;;; Emacs Bookmark Format Version 1 ;;;;
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("B"
+ (filename . "~/Downloads/redis/src/adlist.h")
+ (front-context-string . "listIter *listGe")
+ (rear-context-string . "istNode *node);\n")
+ (position . 2974))
+("A"
+ (filename . "~/Downloads/redis/src/adlist.h")
+ (front-context-string . "/* Functions imp")
+ (rear-context-string . "g len;\n} list;\n\n")
+ (position . 2170))
+)
