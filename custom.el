6e43ba8b0a421f0428f0ee7de103cd54caebad74
yanmingwang <yanming.wang77@gmail.com>
2012-10-16 22:40:03 +0800

    add some code

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (sanityinc-solarized-light)))
- '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(custom-safe-themes (quote ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(session-use-package t nil (session))
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
